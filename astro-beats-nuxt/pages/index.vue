dfd2cbb446a6160da1ce54312a29a74367a06f15
tim-rayner <tim.rayner2020@gmail.com>
2024-03-31 16:03:54 +0100

    feat: added routing from home page

--- astro-beats-nuxt/pages/index.vue
+++ astro-beats-nuxt/pages/index.vue
@@ -1,11 +1,18 @@
 <script setup lang="ts">
 import { StarSigns } from "../constants/starsign-constants";
+
+const router = useRouter();
 </script>
 
 <template>
   <h1>Welcome</h1>
   <div class="flex flex-wrap">
-    <div v-for="sign in StarSigns" :key="sign.id" class="p-4">
+    <div
+      v-for="sign in StarSigns"
+      :key="sign.id"
+      class="p-4"
+      @click="router.push(`/${sign.name}`)"
+    >
       <h2>{{ sign.name }}</h2>
       <p>{{ sign.description }}</p>
     </div>
