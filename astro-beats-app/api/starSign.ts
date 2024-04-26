dae4b0ddb383ad9cd9c2222518ea3aa7548827fa
tim-rayner <tim.rayner2020@gmail.com>
2024-04-26 22:28:50 +0100

    fix: fixed loading async/await issues with horoscope reading 'starsign' screen

--- astro-beats-app/api/starSign.ts
+++ astro-beats-app/api/starSign.ts
@@ -1,15 +1,52 @@
 // src/apis/productAPI.js
 
 export const starSignApi = {
-  post: async function (
+  getHoroscope: async function (
+    starSign: string,
+    // spotifyClientAccessToken: any,
+  ): Promise<any> {
+    // console.log('api recieved spotify token: ', spotifyClientAccessToken);
+    console.log('api recieved star sign: ', starSign);
+
+    const response = await fetch(
+      `https://astro-beats-api.vercel.app/api/horoscopes/V2/reading/${starSign}`,
+      {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        // body: JSON.stringify({
+        //   spotifyClientAccessToken: spotifyClientAccessToken,
+        // }),
+      },
+    );
+
+    if (!response.ok) {
+      console.error('HTTP error', response.status);
+      return new Error('HTTP error');
+    }
+
+    if (response.status === 404) {
+      console.error('404 error', response.status);
+      return new Error('404 error');
+    }
+
+    const data = await response.json();
+    // console.warn('response', data);
+
+    return data;
+  },
+
+  getSongs: async function (
     starSign: string,
     spotifyClientAccessToken: any,
+    horoscopeReading: string,
   ): Promise<any> {
     console.log('api recieved spotify token: ', spotifyClientAccessToken);
     console.log('api recieved star sign: ', starSign);
 
     const response = await fetch(
-      `https://astro-beats-api.vercel.app/api/horoscopes/${starSign}`,
+      `https://astro-beats-api.vercel.app/api/horoscopes/V2/songs/${starSign}`,
       {
         method: 'POST',
         headers: {
@@ -17,6 +54,7 @@ export const starSignApi = {
         },
         body: JSON.stringify({
           spotifyClientAccessToken: spotifyClientAccessToken,
+          horoscopeReading: horoscopeReading,
         }),
       },
     );
--- astro-beats-app/app/starSign/[starSign].tsx
+++ astro-beats-app/app/starSign/[starSign].tsx
@@ -46,40 +46,76 @@ const Error = () => {
 
 const StarSignScreen = () => {
   const [horoscope, setHoroscope] = useState<any>();
+  const [songs, setSongs] = useState<any>();
+
+  const [spotifyToken, setSpotifyToken] = useState<any>();
 
   const [error, setError] = useState<boolean>(false);
   const [loadingHoroScope, setLoadingHoroscope] = useState<boolean>(false);
+  const [loadingSongs, setLoadingSongs] = useState<boolean>(false);
 
   const { starSign } = useLocalSearchParams();
+
+  //get token from secure store, then get horoscope, then get songs
+  const fetchStarSignData = useCallback(async () => {
+    try {
+      const token = await fetchAuthToken();
+      const horoscopeData = await fetchHoroscope();
+      console.log('horoscopeData', horoscopeData);
+      await fetchSongs(token, horoscopeData.horoscope);
+    } catch (error) {
+      console.error(error);
+    }
+  }, []);
 
   const fetchAuthToken = useCallback(async () => {
     const token = await secureAuthStore.getTokenAsync();
     if (token) {
-      fetchHoroscope(token);
+      setSpotifyToken(token);
     }
+    return token;
   }, []);
 
-  const fetchHoroscope = useCallback(async (token) => {
+  const fetchHoroscope = useCallback(async () => {
     setLoadingHoroscope(true);
-    await starSignApi
+    const resp = await starSignApi
       // @ts-ignore
-      .post(starSign, token)
+      .getHoroscope(starSign)
       .then((response) => {
-        setHoroscope({
+        const horoscope = {
           horoscope: response.horoscopeReading,
           date: response.date,
-          songs: response.songs,
-        });
+        };
+
+        setHoroscope(horoscope);
+        return horoscope;
       })
       .catch((error) => {
         console.warn('error', error);
         setError(true);
       });
     setLoadingHoroscope(false);
+    return resp;
+  }, []);
+
+  const fetchSongs = useCallback(async (token, reading) => {
+    setLoadingSongs(true);
+    await starSignApi
+      // @ts-ignore
+      .getSongs(starSign, token, reading)
+      .then((response) => {
+        console.log('response', response);
+        setSongs(response);
+      })
+      .catch((error) => {
+        console.warn('error', error);
+        setError(true);
+      });
+    setLoadingSongs(false);
   }, []);
 
   useEffect(() => {
-    fetchAuthToken();
+    fetchStarSignData();
   }, []);
 
   return (
@@ -87,6 +123,8 @@ const StarSignScreen = () => {
       <Text>Star Sign Screen for {starSign}</Text>
       {loadingHoroScope && <LoadingHoroscope />}
       {horoscope && <Horoscope horoscope={horoscope} />}
+      {loadingSongs && <LoadingSongs />}
+      {songs && <Text>{JSON.stringify(songs)}</Text>}
       {error && <Error />}
     </SafeAreaView>
   );
