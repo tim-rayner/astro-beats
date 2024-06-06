ba7263e5b7142b27884206d423561c9c7b39a30d
tim-rayner <tim.rayner2020@gmail.com>
2024-06-06 18:23:08 +0100

    update: updated wrapper of track card

--- astro-beats-nuxt/components/starsign/Track.vue
+++ astro-beats-nuxt/components/starsign/Track.vue
@@ -11,32 +11,36 @@ const { track, activeIndex, index } = defineProps<Props>();
 </script>
 
 <template>
-  <div
-    class="card my-6 shadow-lg bg-primary rounded-3xl text-[#161937] flex flex-col items-center justify-center p-3 w-[90%] lg:max-w-[600px] mx-4 pb-12"
-  >
-    <div class="title text-xl font-bold">{{ track.song }}</div>
-    <div class="artist text-lg text-left font-semibold">{{ track.artist }}</div>
-
-    <div class="img-container h-48 my-4">
-      <img
-        class="w-full h-full object-cover rounded-full shadow-lg"
-        :src="track.img"
-        alt="Album cover"
-      />
-    </div>
-
-    <div class="player-wrapper w-full">
-      <StarsignPlayer :track="track" />
-    </div>
-  </div>
-
-  <transition name="fade" mode="out-in">
+  <div>
     <div
-      class="reason text-lg text-left font-semibold mx-4 mb-12 px-5 bg-background border border-primary rounded-lg shadow-lg p-4 lg:max-w-[600px]"
+      class="card my-6 shadow-lg bg-primary rounded-3xl text-[#161937] flex flex-col items-center justify-center p-3 w-[90%] lg:max-w-[600px] mx-4 pb-12"
     >
-      {{ track.reason }}
+      <div class="title text-xl font-bold">{{ track.song }}</div>
+      <div class="artist text-lg text-left font-semibold">
+        {{ track.artist }}
+      </div>
+
+      <div class="img-container h-48 my-4">
+        <img
+          class="w-full h-full object-cover rounded-full shadow-lg"
+          :src="track.img"
+          alt="Album cover"
+        />
+      </div>
+
+      <div class="player-wrapper w-full">
+        <StarsignPlayer :track="track" />
+      </div>
     </div>
-  </transition>
+
+    <transition name="fade" mode="out-in">
+      <div
+        class="reason text-lg text-left font-semibold mx-4 mb-12 px-5 bg-background border border-primary rounded-lg shadow-lg p-4 lg:max-w-[600px]"
+      >
+        {{ track.reason }}
+      </div>
+    </transition>
+  </div>
 </template>
 
 <style>
